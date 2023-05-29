a32f2a9961e0497af7c78343c6dd82b1c73612ac
AnastasiaPanysh <nastya.panysh@gmail.com>
2023-05-29 23:02:53 +0300

    add 1,2 tasks

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Counter from "./Page/Counter";
+import Math from "./Page/Math";
+
+function App() {
+
+  return (
+    <>
+      <Counter></Counter>
+      <Math></Math>
+    </>
+  );
+}
+
+export default App;
